ca521a03a594bd75a5890eb4b7ffa63b47e5d5fa
zaiying fan <zaiying.fan@realai.ai>
2024-07-29 15:15:48 +0800

    feat: computed实现

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,5 +1,6 @@
 import { isArray } from '@vue/shared'
 import { createDep, Dep } from './dep'
+import { ComputedRefImpl } from './computed'
 
 type KeyToDepMap = Map<any, Dep>
 /**
@@ -77,8 +78,20 @@ export function triggerEffects(dep: Dep) {
   // 把dep构建为一个数组(Array.isArray(new Set())) => false
   const effects = isArray(dep) ? dep : [...dep]
   // 依次触发
+  // for (const effect of effects) {
+  //   triggerEffect(effect)
+  // }
+
+  // 不在依次触发，而是先触发所有的计算依赖属性，再触发所有的非计算属性依赖
   for (const effect of effects) {
-    triggerEffect(effect)
+    if (effect.computed) {
+      triggerEffect(effect)
+    }
+  }
+  for (const effect of effects) {
+    if (!effect.computed) {
+      triggerEffect(effect)
+    }
   }
 }
 
@@ -86,10 +99,17 @@ export function triggerEffects(dep: Dep) {
  * 触发置顶的依赖
  */
 export function triggerEffect(effect: ReactiveEffect) {
-  // 这里又会执行一次依赖收集
-  // 比如：effect(() => {document.querySelector('#p1').innerText = obj.name})触发getter
-  // 但是key: set<ReactiveEffect>结构会去重
-  effect.run()
+  if (effect.scheduler) {
+    // 存在调度器就执行调度函数
+    effect.scheduler()
+  } else {
+    // 这里又会执行一次依赖收集
+    // 比如：effect(() => {document.querySelector('#p1').innerText = obj.name})触发getter
+    // 但是key: set<ReactiveEffect>结构会去重
+
+    // 否则直接执行run函数即可
+    effect.run()
+  }
 }
 
 /**
@@ -113,18 +133,28 @@ export function effect<T = any>(fn: () => T) {
  */
 export let activeEffect: ReactiveEffect | undefined
 
+export type EffectScheduler = (...args: any[]) => any
+
 /**
  * 响应性触发依赖时的执行类
  */
 export class ReactiveEffect<T = any> {
-  // fnStr: string
+  /**
+   * 存在该属性，则表示当前的effect为计算属性的effect
+   */
+  computed?: ComputedRefImpl<T>
+
+  fnStr: string
 
   // constructor: 这是类的构造函数，用于初始化类的新实例。
   // public fn: 这是一个公共（public）属性 fn 的声明，它直接在构造函数的参数列表中被初始化。这意味着 fn 将成为类的一个公共属性，可以通过类的实例直接访问。
   // () => T: 这定义了 fn 的类型。fn 必须是一个无参数的函数，并且它的返回类型是由泛型 T 指定的。泛型 T 可以在类的使用过程中被指定为任何具体的类型。
   // 整体来说，这段代码的作用是在创建类的新实例时，需要传入一个无参数的函数，该函数的返回值类型是泛型 T。这个函数会被保存为类实例的 fn 属性，可以在后续的代码中调用。
-  constructor(public fn: () => T) {
-    // this.fnStr = fn.toString()
+  constructor(
+    public fn: () => T,
+    public scheduler: EffectScheduler | null = null
+  ) {
+    this.fnStr = fn.toString()
   }
 
   run() {
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -14,3 +14,9 @@ export const isObject = (val: unknown) =>
  */
 export const hasChanged = (value: any, oldValue: any): boolean =>
   !Object.is(value, oldValue)
+
+/**
+ * 是否为一个function
+ */
+export const isFunction = (val: unknown): val is Function =>
+  typeof val === 'function'
