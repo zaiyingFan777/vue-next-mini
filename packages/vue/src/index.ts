44e0bac90b8ed5a14ca7f2df18640f50ca229909
zaiying.fan <zaiying.fan@outlook.com>
2026-01-20 11:31:29 +0800

    feat: 1.实现reactive和effect

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -15,10 +15,10 @@
 // export * from '@vue/shared'
 
 export * from '@vue/reactivity'
-export * from '@vue/runtime-core'
+// export * from '@vue/runtime-core'
 
-export * from '@vue/runtime-dom'
+// export * from '@vue/runtime-dom'
 
-export * from '@vue/vue-compat'
+// export * from '@vue/vue-compat'
 
-export * from '@vue/shared'
+// export * from '@vue/shared'
