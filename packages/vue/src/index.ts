f1eb6a7d829f61e38b3360b3c5ea304a761bbc1e
zaiying.fan <zaiying.fan@outlook.com>
2026-02-13 13:25:34 +0800

    feat: compile

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -12,12 +12,13 @@
 // export { render } from '@vue/runtime-dom'
 // // export { compile } from '@vue/compiler-dom'
 // export { compile } from '@vue/vue-compat'
+// // export * from '@vue/vue-compat'
 // export * from '@vue/shared'
 
+// 第二版
 export * from '@vue/reactivity'
 export * from '@vue/runtime-core'
 export * from '@vue/runtime-dom'
+export * from '@vue/vue-compat'
+export * from '@vue/shared'
 
-// export * from '@vue/vue-compat'
-
-// export * from '@vue/shared'
